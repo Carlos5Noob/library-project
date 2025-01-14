98e34b7981c65ee0a718805243a57c685f5f8fd1
Carlos5Noob <cchaati2101@fp.iesromerovargas.com>
2025-01-14 10:53:43 +0100

    pagina autores hecha

--- books/urls.py
+++ books/urls.py
@@ -7,4 +7,5 @@ app_name = 'books'
 urlpatterns = [
     path("", views.index, name='index'),
     path("<int:book_id>/", views.detail, name='detail'),
+    path("autores/", views.author, name='author'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 
-from .models import Book
+from .models import Book, Author
+
 
 # Create your views here.
 def index(request):
@@ -11,3 +12,7 @@ def index(request):
 def detail(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
     return render(request, 'books/detail.html', context={'book': book})
+
+def author(request):
+    autores = Author.objects.all()
+    return render(request, "books/autores.html", context={"autores": autores})
