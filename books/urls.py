80956643d0ed733e6ea80565bf85525f51695be1
Carlos5Noob <cchaati2101@fp.iesromerovargas.com>
2025-01-14 19:15:07 +0100

    vista de géneros y filtrar libros por géneros hecho

--- books/urls.py
+++ books/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path("<int:book_id>/", views.detail, name='detail'),
     path("autores/", views.author, name='author'),
     path("autores/<int:author_id>/", views.author_detail, name='author_detail'),
+    path("generos/", views.genre, name='genero'),
+    path("<str:genre_name>/", views.genre_detail, name='genre_detail'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 
-from .models import Book, Author
+from .models import Book, Author, Genre
 
 
 # Create your views here.
@@ -21,3 +21,12 @@ def author_detail(request, author_id):
     autor = get_object_or_404(Author, pk=author_id)
     libros = Book.objects.filter(authors=autor)
     return render(request, template_name="books/autordetail.html", context={"autor": autor, "libros": libros})
+
+def genre(request):
+    generos = Genre.objects.all()
+    return render(request, template_name="books/generos.html", context={"generos": generos})
+
+def genre_detail(request, genre_name):
+    genre = get_object_or_404(Genre, name=genre_name)
+    libros = Book.objects.filter(genre=genre)
+    return render(request, template_name="books/librosgen.html", context={"genre": genre, "libros": libros})
