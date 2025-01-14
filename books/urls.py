a06d3123c11140daff0f7ad7379e96d01c85abc4
Carlos5Noob <cchaati2101@fp.iesromerovargas.com>
2025-01-14 10:35:27 +0100

    pagina principal hecha y panel de administracion actualizado

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
 
-from books import views
+from . import views
 
 app_name = 'books'
 urlpatterns = [
+    path("", views.index, name='index'),
 
 ]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
+from .models import Book
 
 # Create your views here.
+def index(request):
+    books = Book.objects.all()
+    return render(request, 'books/index.html', context={'books': books})
