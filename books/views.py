96dad897b7ed731fb39ba8acdfef9ab229c61a9d
Carlos5Noob <cchaati2101@fp.iesromerovargas.com>
2025-01-24 21:21:36 +0100

    views.py refactored

--- books/views.py
+++ books/views.py
@@ -1,37 +1,36 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
+
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
-from django.urls import reverse
 
-from django.contrib.auth import authenticate, login, logout
-import books
+
+
 from .models import Book, Author, Genre
 
 
 # Create your views here.
-@login_required(login_url='login')
+@login_required
 def index(request):
     books = Book.objects.all()
     return render(request, 'books/index.html', context={'books': books})
 
-@login_required(login_url='login')
+@login_required
 def detail(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
     return render(request, 'books/detail.html', context={'book': book})
 
-@login_required(login_url='login')
+@login_required
 def author(request):
     autores = Author.objects.all()
     return render(request, "books/autores.html", context={"autores": autores})
 
-@login_required(login_url='login')
+@login_required
 def author_detail(request, author_id):
     autor = get_object_or_404(Author, pk=author_id)
     libros = Book.objects.filter(authors=autor)
     return render(request, template_name="books/autordetail.html", context={"autor": autor, "libros": libros})
 
-@login_required(login_url='login')
+@login_required
 def genre(request):
     generos = Genre.objects.all()
     libros_terror = Book.objects.filter(genre = 1)
@@ -48,13 +47,13 @@ def genre(request):
         cont_d+=1
     return render(request, template_name="books/generos.html", context={"generos": generos, "libros_terror": cont_t, "libros_deportes": cont_d, "libros_comedia": cont_c})
 
-@login_required(login_url='login')
+@login_required
 def genre_detail(request, genre_name):
     genre = get_object_or_404(Genre, name=genre_name)
     libros = Book.objects.filter(genre=genre)
     return render(request, template_name="books/librosgen.html", context={"genre": genre, "libros": libros})
 
-@login_required(login_url='login')
+@login_required
 def form(request):
     if request.method == "POST":
         first_name = request.POST["first_name"]
@@ -69,7 +68,7 @@ def form(request):
 
     return render(request, template_name="books/formulario.html")
 
-@login_required(login_url='login')
+@login_required
 def form2(request):
     autores = Author.objects.all()
     genres = Genre.objects.all()
@@ -102,7 +101,7 @@ def form2(request):
 
     return render(request, "books/formulario2.html", {"autores": autores, "genres": genres})
 
-@login_required(login_url='login')
+@login_required
 def recientes(request):
     libros = Book.objects.order_by("-publish_date")[:5]
     return render(request, template_name="books/recientes.html", context={"libros": libros})
@@ -110,19 +109,3 @@ def recientes(request):
 def jonatan(request):
     return render(request, template_name="books/jonatan.html")
 
-def login_view(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(reverse('books:index'))
-        else:
-            return render(request, "books/login.html", context={"error": "Invalid username or password"})
-    return render(request, template_name="books/login.html")
-
-def logout_view(request):
-    logout(request)
-    return HttpResponseRedirect(reverse('login'))
